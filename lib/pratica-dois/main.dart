f1205e5943dcbc9f3b2d72751e7b5e9c78cb96e8
Luiz Antunes <antunesluiz27@gmail.com>
2021-08-27 17:14:32 -0300

    commit prática 02 e 03

---
+++ lib/pratica-dois/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Luiz Antunes',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      ), //Text.
+    ), //Center.
+  ); //runApp.
+}
